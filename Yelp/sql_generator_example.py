c4811aa515f35632f6512f3a8c858bc47c7eb316
Jaehoon Jung <jkjung@cs.uchicago.edu>
2016-02-17 17:40:35 -0600

    yeee

--- Yelp/sql_generator_example.py
+++ Yelp/sql_generator_example.py
@@ -7,19 +7,15 @@ XMAX = 41.802335
 YMIN = -87.596666
 YMAX = -87.584138
 
-db = sqlite3.connect("restaurants.sqlite")
+db = sqlite3.connect("restaurants.db")
 
 c = db.cursor()
 
-c.execute("DROP TABLE time")
-c.execute("DROP TABLE yelp")
-c.execute("DROP TABLE maps")
-
-time = "CREATE TABLE time (m_open, m_closed, t_open, t_closed, "
-time += "w_open, w_closed, r_open, r_closed, f_open, f_closed, "
-time += "sat_open, sat_closed, sun_open, sun_closed, name_id);"
-yelp = "CREATE TABLE yelp (name_id, price, rating);"
-maps = "CREATE TABLE maps (lon, lat, name_id);"
+time = "CREATE TABLE time (m_open int, m_closed int, t_open int, t_closed int, "
+time += "w_open int, w_closed int, r_open int, r_closed int, f_open int, f_closed int, "
+time += "sat_open int, sat_closed int, sun_open int, sun_closed int, name_id varchar(30));"
+yelp = "CREATE TABLE yelp (name_id varchar(30), price int, rating float);"
+maps = "CREATE TABLE maps (lon float, lat float, name_id varchar(30));"
 
 timelist = "(m_open, m_closed, t_open, t_closed , w_open, w_closed, r_open, r_closed, f_open, f_closed, \
     sat_open, sat_closed, sun_open, sun_closed, name_id)"
@@ -51,14 +47,10 @@ for i in range(5):
     args_time[i] = tuple(args_time[i])
     args_maps[i] = tuple(args_maps[i])
 
-print(args_yelp)
-print(args_time)
-print(args_maps)
-
 for i in range(5):
     c.execute("INSERT INTO yelp (name_id, price, rating)  VALUES (?, ?, ?);", args_yelp[i])
     c.execute("INSERT INTO time" + timelist + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", args_time[i])
     c.execute("INSERT INTO maps VALUES (?, ?, ?);", args_maps[i])
-
+db.commit()
 db.close()
 
