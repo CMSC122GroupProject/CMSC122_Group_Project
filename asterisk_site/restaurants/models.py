07451733acd8bfc0760ca237c1b2b819d3ea4617
Adam Sudit <asudit@cs.uchicago.edu>
2016-02-29 15:26:48 -0600

    some morning progress

--- asterisk_site/restaurants/models.py
+++ asterisk_site/restaurants/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.utils import timezone
-
+import geocoder
 
 class Dine_query(models.Model):
     name = models.CharField(max_length=64)
@@ -10,10 +10,32 @@ class Dine_query(models.Model):
     closing_time = models.IntegerField()
     day = models.CharField(max_length=10)
     distance = models.FloatField()
+    current_location = models.CharField(max_length=100)
+    current_city = models.CharField(max_length=20)
     created_date = models.DateTimeField(default=timezone.now)
+    longitude = None
+    latitude = None
+
 
     def __repr__(self):
         return '{}: Price: {}, Desired Rating: {}, Opening Time: {}, Closing Time: {}, Distance: {}'.format(self.name, self.price, self.desired_rating, self.opening_time, self.closing_time, self.distance)
 
     def __str__(self):
         return '{}: Price: {}, Desired Rating: {}, Opening Time: {}, Closing Time: {}, Distance: {}'.format(self.name, self.price, self.desired_rating, self.opening_time, self.closing_time, self.distance)
+
+    def get_lon_lat(self):
+        address = self.current_location
+        city = self.current_city
+        g = geocoder.google(",".join(("address", "city")))
+        (self.longitude, self.latitude) = g.latlng
+        return self.longitude, self.latitude
+
+
+
+        '''
+        geolocator = Nominatim()
+        address = self.current_location
+        city = self.current_city
+        location = geolocator.geocode("address" + "city")  
+        (self.longitude, self.latitude) = (location.longitude, location.latitude)
+        '''
--- asterisk_site/restaurants/views.py
+++ asterisk_site/restaurants/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Dine_query
 from .forms import DineQueryForm
+from .asterisk_pre_algo import dict_api, desired_output, tables, dict_what, DATABASE_FILENAME
+from .asterisk_pre_algo import query_relations, query_join, query_where, query_select, prelim_assembly, prelim_algorithm, algorithm
 #from  import algorithm
 
 import sqlite3
 import os
 import re
-DATABASE_FILENAME = '/home/student/cs122-win-16-asudit/CMSC122_Group_Project/asterisk_site/restaurants.db'
-DATA_DIR = os.path.dirname(__file__)
+#DATABASE_FILENAME = '/home/student/cs122-win-16-asudit/CMSC122_Group_Project/asterisk_site/restaurants.db'
+#DATA_DIR = os.path.dirname(__file__)
 #DATABASE_FILENAME = os.path.join(DATA_DIR, 'restaurants/restaurants.db')
 day_dict = {'Monday': ('m_open', 'm_closed'), 'Tuesday': ('t_open', 't_closed'), 'Wednesday': ('w_open', 'w_closed'), 'Thursday': ('r_open', 'r_closed'),
                 'Friday' : ('f_open', 'f_closed'), 'Saturday' : ('sat_open', 'sat_closed'), 'Sunday' : ('sun_open', 'sun_closed')}
@@ -24,8 +26,11 @@ def dine_query_list(request):
         day = query.day
         print(day)
         times = day_dict[day]
+        #lat = query.latitude
+        #ssslon = query.longitude
+        lon, lat = query.get_lon_lat()
         sample = {'name_id': query.name, 'price': query.price, 'rating': query.desired_rating , times[0] : query.opening_time, times[1] : query.closing_time, 
-                'preferences' : ['name_id', 'distance', 'price', 'rating', times[0], times[1] ] }
+                'lat': lat, 'lon' : lon, 'preferences' : ['name_id', 'distance', 'price', 'rating', times[0], times[1] ] }
         algo = algorithm(sample)
         results.append((query, algo))
         print('it worked!!!!!!!!!!!!!' , results)
@@ -64,154 +69,4 @@ def dine_query_algo(request):
 '''
 
 
-
-
-
-dict_api = {'yelp' : ['name_id', 'price', 'rating'], 'time' : ['m_open', 'm_closed', 't_open', 't_closed', 'w_open', 'w_closed', 'r_open', 'r_closed', 'f_open',
-            'f_closed', 'sat_open', 'sat_closed', 'sun_open', 'sun_closed', 'name_id'], 'maps' : ['lon', 'lat', 'name_id']}
-
-#not all of the attributes in our sample input would be included in output-some will be in the where statment etc
-desired_output = ['name_id']
-
-#some tables more important than others-eg yelp table more important than twitter etc
-tables = ['yelp', 'time', 'maps']
-
-#paramters for where statment in SQL query
-dict_what = {'price' : ['yelp.price' + '<=' + '?']}
-dict_what['distance'] = ['maps.distance' + '<=' + '?']
-dict_what['rating'] = ['yelp.rating' + '>=' + '?']
-dict_what['m_open'] = ['time.m_open' + '>=' + '?']
-dict_what['m_closed'] = ['time.m_closed' + '<=' + '?']
-dict_what['t_open'] = ['time.m_open' + '>=' + '?']
-dict_what['t_closed'] = ['time.t_closed' + '<=' + '?']
-dict_what['w_open'] = ['time.w_open' + '>=' + '?']
-dict_what['w_closed'] = ['time.w_closed' + '<=' + '?']
-dict_what['r_open'] = ['time.r_open' + '>=' + '?']
-dict_what['r_closed'] = ['time.r_closed' + '<=' + '?']
-dict_what['f_open'] = ['time.f_open' + '>=' + '?']
-dict_what['f_closed'] = ['time.f_closed' + '<=' + '?']
-dict_what['sat_open'] = ['time.sat_open' + '>=' + '?']
-dict_what['sat_closed'] = ['time.sat_closed' + '<=' + '?']
-dict_what['sun_open'] = ['time.sun_open' + '>=' + '?']
-dict_what['sun_closed'] = ['time.sun_closed' + '<=' + '?']
-dict_what['name_id'] = ['yelp.name_id'  + '=' + '?']
-
-
-
-
-
-def query_relations(sample):
-    relation_list = []
-    relations = list(dict_api.keys())
-    samples = list(sample.keys())
-    for param in samples:
-        for table in tables:
-            if param in dict_api[table] and table in relation_list:
-                break
-            elif param in dict_api[table] and table not in relation_list:
-                relation_list.append(table)
-
-    return  relation_list
-
-def query_join(sample):
-    relation = query_relations(sample)
-    join_on_list = []
-    for table in relation:
-        other = [i for i in relation if i != table]
-        #print(other)
-        for i in other:
-            keys1 = set(dict_api[i])
-            keys2 = set(dict_api[table])
-            #print(keys1, keys2)
-            key = list(keys1.intersection(keys2))
-            #print(key)
-            if key != []:
-                    join1 = ".".join((table, key[0]))
-                    join2 = ".".join((i, key[0]))
-                    #print(join1, join2)
-                    if table < i:
-                        if ((join1, join2), (table, i)) not in join_on_list:
-                            join_on_list.append(((join1, join2), (table, i)))
-                    else:
-                        if ((join2, join1), (i, table)) not in join_on_list:
-                            join_on_list.append(((join2, join1), (i, table)))
-    return join_on_list
-
-
-def query_select(sample):
-    relations = query_relations(sample)
-    select_list = []
-    count = []
-    samples = list(sample.keys())
-    for param in samples:
-        for table in relations:
-            if param in dict_api[table] and param in desired_output and param not in count:
-                select_list.append(".".join((table, param)))
-                count.append(param)
-    return select_list
-
-
-def query_where(sample):
-    parameters = list(dict_what.keys())
-    inputs = sample['preferences']
-    what_list = []
-    where_param = []
-    for i in inputs:
-        if i in parameters:
-            if i == 'distance':
-                pass
-            else:
-                what_list.append(dict_what[i])
-                where_param.append(i)
-    return what_list, where_param
-
-def prelim_assembly(sample):
-    SELECT = 'SELECT' + " "  + ",".join(query_select(sample))
-    FROM = 'FROM' + " " + " JOIN ".join(query_relations(sample))
-    on_list = []
-    on_list_filter = []
-    for i in query_join(sample):
-        on_list.append(i[0])
-    for i in on_list:
-        on_list_filter.append("=".join(i))
-    ON = "ON" + " " + " AND ".join(on_list_filter)
-    where = []
-    for i in query_where(sample)[0]:
-        where = where + i
-    WHERE = "WHERE" + " " + " AND ".join(where)
-    query = SELECT + " " + FROM + " " + ON + " " + WHERE
-    quest_which = []
-    for i in query_where(sample)[1]:
-        quest_which.append(sample[i])
-    return query, quest_which
-
-
-def prelim_algorithm(sample):
-    print(DATABASE_FILENAME)
-    db = sqlite3.connect(DATABASE_FILENAME)
-    c = db.cursor()
-    s = prelim_assembly(sample)[0]
-    args = prelim_assembly(sample)[1]
-    #print(s, args)
-    r = c.execute(s, args)
-    result = r.fetchall()
-    
-    #db.close()
-    return result
-
-def algorithm(sample):
-    query_return = prelim_algorithm(sample)
-    #print('query return:', query_return)
-    if len(sample['preferences']) == 0:
-        #print('end reached')
-        return []
-    if query_return != []:
-        #print('best case')
-        return query_return
-    else:
-        least = sample['preferences'][-1]
-        sample.pop(least, None)
-        sample['preferences'].remove(least)
-        #print('rework')
-        return algorithm(sample)
         
